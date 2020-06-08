2a242fe8b59a3b082581149be55edc03a1518c7d
MathieuBoe <mathieuboe60@gmail.com>
2020-06-08 11:21:29 +0200

    Exercice complet

---
+++ Exercice7.php
@@ -0,0 +1,10 @@
+<?php
+$age = 16;
+$gender = 'Homme';
+$title = 'Exercice7';
+
+include 'header.php';
+include '../Exercice5/Exercice5.php';
+$departement [51] = 'Marne';
+var_dump($departement);
+include 'footer.php';?>
